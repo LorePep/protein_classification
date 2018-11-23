61c4767f5b1293cbbcd9a44c8905eadb027c1e63
Lorenzo Peppoloni <lorenzo@Lorenzos-MacBook-Air.local>
2018-11-23 18:11:29 +0000

    create rgb

---
+++ main.py
@@ -0,0 +1,54 @@
+import argparse
+import os
+import sys
+from tqdm import tqdm
+
+from PIL import Image
+
+from preprocessing import (
+    get_images_prefixes,
+    get_rgb_to_prefixes)
+
+
+DEFAULT_WIDTH = 512
+DEFAULT_HEIGHT = 512
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Preprocess images.")
+    parser.add_argument("input", type=str, help="dataset input path")
+    parser.add_argument("--output-path", "-o", type=str, help="preprocessing output path", required=True)
+    parser.add_argument("--create-rgb", dest="create", action="store_true",
+                        help="create rgb for images")
+
+    args = parser.parse_args()
+    validate_args(args)
+
+    if args.create:
+        create_rgbs(args.input)
+    else:
+        print("Nothing to do.")
+
+
+def create_rgbs(input_path: str):
+    paths = [os.path.join(input_path, f) for f in os.listdir(input_path) if f.endswith(".png")]
+    prefixes = get_images_prefixes(paths)
+    prefixes_to_rgb = get_rgb_to_prefixes(prefixes, DEFAULT_WIDTH, DEFAULT_HEIGHT)
+
+    for prefix, rgb in tqdm(prefixes_to_rgb.items(), info="saving images on disk", unit="images"):
+        im = Image.fromarray(rgb)
+        base = os.path.basename(prefix)
+        rgb_path = os.path.join(args.output_path, base + ".png")
+        im.save(rgb_path)
+
+
+def validate_args(args):
+    if not os.path.isdir(args.output_path):
+        print("Specified output path {} is not a directory".format(args.output_path))
+        sys.exit()
+    
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ preprocessing.py
@@ -0,0 +1,55 @@
+import os
+from multiprocessing.pool import ThreadPool
+from tqdm import tqdm
+
+import numpy as np
+from PIL import Image
+
+RGB_CHANNEL_TO_INDEX = {"red": 0, "green": 1, "blue": 2}
+NUM_PROCESSES = 4
+
+
+def get_images_prefixes(images_paths: list) ->list:
+    """
+    >>> get_images_prefixes(["one-path_aaa.png", "another-path_aaa.png", "one-path_bbb.png"])
+    >>> ['one-path', 'another-path']
+    >>> get_images_prefixes(["this-path_is-not_fine"])
+    Traceback (most recent call last):
+    ...
+    ValueError: uknown path format 'this-path_is-not_fine'
+    """
+    prefixes = set()
+    for path in images_paths:
+        split_path = path.split("_")
+        if len(split_path) != 2:
+            raise ValueError("uknown path format %s", path)
+        prefixes.add(split_path[0])
+
+    return list(prefixes)
+        
+    
+def get_rgb_to_prefixes(prefixes: list, width: int, height: int) -> dict:
+    prefixes_to_rgb = {}
+
+    def f(prefix):
+        prefixes_to_rgb[prefix] = get_rgb(prefix, width, height)
+
+    
+    pool = ThreadPool(processes=NUM_PROCESSES)
+    with tqdm(total=len(prefixes), info="creating rgb images.", unit="images") as pbar:
+        for i, _ in tqdm(enumerate(pool.imap(f, prefixes))):
+            pbar.update()
+
+    return prefixes_to_rgb
+
+
+def get_rgb(prefix: str, width, height) -> np.ndarray:
+    rgb_image = np.zeros(shape=(height, width, 3), dtype=np.float)
+
+    for channel, idx in RGB_CHANNEL_TO_INDEX.items():
+        current_image = Image.open(prefix + "_" + channel + ".png")
+        rgb_image[:, :, idx] = current_image
+    
+    rgb_image = rgb_image / rgb_image.max() * 255
+    return rgb_image.astype(np.uint8)
+
